7b16003dbba543c02840fc3184b7ae6f0c3029be
stanman88 <71804470+stanman88@users.noreply.github.com>
2020-10-17 21:41:40 +0800

    Added First 10 Counters

--- MACD Manual.py
+++ MACD Manual.py
@@ -2,22 +2,23 @@ from Alphas.MacdAlphaModel import MacdAlphaModel
 from Execution.ImmediateExecutionModel import ImmediateExecutionModel
 from Portfolio.EqualWeightingPortfolioConstructionModel import EqualWeightingPortfolioConstructionModel
 
-class OptimizedModulatedCoreWave(QCAlgorithm):
+class TachyonParticleAtmosphericScrubbers(QCAlgorithm):
 
     def Initialize(self):
         self.SetStartDate(2020, 4, 16)  # Set Start Date
+        self.SetEndDate(2020, 4, 16)    # Set End Date
         self.SetCash(100000)  # Set Strategy Cash
         # self.AddEquity("SPY", Resolution.Minute)
-        self.AddAlpha(MacdAlphaModel(12, 26, 9, MovingAverageType.Simple, Resolution.Daily))    #MACD
+        self.AddAlpha(MacdAlphaModel(12, 26, 9, MovingAverageType.Simple, Resolution.Daily))
 
-        self.SetExecution(ImmediateExecutionModel())    #Immediate execution
+        self.SetExecution(ImmediateExecutionModel())
 
-        self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())   #equal weight
+        self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
 
     
-        self.SetRiskManagement(MaximumUnrealizedProfitPercentPerSecurity(0.03)) #max unrealised profit
-#Manually add stock counters in here
-        symbols = [Symbol.Create("SPY", SecurityType.Equity, Market.USA),  Symbol.Create("AAPL", SecurityType.Equity, Market.USA)]
+        self.SetRiskManagement(MaximumUnrealizedProfitPercentPerSecurity(0.03))
+#Add Counters here
+        symbols = [Symbol.Create("GOOG", SecurityType.Equity, Market.USA),  Symbol.Create("AAPL", SecurityType.Equity, Market.USA),Symbol.Create("BABA", SecurityType.Equity, Market.USA),Symbol.Create("V", SecurityType.Equity, Market.USA),Symbol.Create("GS", SecurityType.Equity, Market.USA),Symbol.Create("GSK", SecurityType.Equity, Market.USA),Symbol.Create("LLY", SecurityType.Equity, Market.USA),Symbol.Create("KHC", SecurityType.Equity, Market.USA),Symbol.Create("FDX", SecurityType.Equity, Market.USA),Symbol.Create("KMB", SecurityType.Equity, Market.USA)]
         self.SetUniverseSelection( ManualUniverseSelectionModel(symbols) )
 
 
@@ -26,3 +27,6 @@ class OptimizedModulatedCoreWave(QCAlgorithm):
             Arguments:
                 data: Slice object keyed by symbol containing the stock data
         '''
+
+        # if not self.Portfolio.Invested:
+        #    self.SetHoldings("SPY", 1)
