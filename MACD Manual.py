944e500c1f4b5ece31a379429cd72aa5b4aabbf7
stanman88 <71804470+stanman88@users.noreply.github.com>
2020-10-17 22:58:57 +0800

    43 PERCENT

--- MACD Manual.py
+++ MACD Manual.py
@@ -5,7 +5,7 @@ from Portfolio.EqualWeightingPortfolioConstructionModel import EqualWeightingPor
 class TachyonParticleAtmosphericScrubbers(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2020, 4, 16)  # Set Start Date
+        self.SetStartDate(2018, 4, 16)  # Set Start Date
         self.SetEndDate(2020, 4, 16)    # Set End Date
         self.SetCash(100000)  # Set Strategy Cash
         # self.AddEquity("SPY", Resolution.Minute)
@@ -16,17 +16,27 @@ class TachyonParticleAtmosphericScrubbers(QCAlgorithm):
         self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel())
 
     
-        self.SetRiskManagement(MaximumUnrealizedProfitPercentPerSecurity(0.01))
+        self.SetRiskManagement(MaximumUnrealizedProfitPercentPerSecurity(0.03))
+
 #Add Counters here
         symbols = [Symbol.Create("GOOG", SecurityType.Equity, Market.USA),  
         Symbol.Create("AAPL", SecurityType.Equity, Market.USA),
-        Symbol.Create("BABA", SecurityType.Equity, Market.USA),
+        Symbol.Create("NFLX", SecurityType.Equity, Market.USA),
+        Symbol.Create("MSFT", SecurityType.Equity, Market.USA),
+        Symbol.Create("EBAY", SecurityType.Equity, Market.USA),
+        Symbol.Create("JPM", SecurityType.Equity, Market.USA),
         Symbol.Create("V", SecurityType.Equity, Market.USA),
-        Symbol.Create("GS", SecurityType.Equity, Market.USA),
-        Symbol.Create("GSK", SecurityType.Equity, Market.USA),
-        Symbol.Create("LLY", SecurityType.Equity, Market.USA),
+        Symbol.Create("BAC", SecurityType.Equity, Market.USA), 
+        Symbol.Create("WMT", SecurityType.Equity, Market.USA),
+        Symbol.Create("SBUX", SecurityType.Equity, Market.USA),
+        Symbol.Create("MCD", SecurityType.Equity, Market.USA),
         Symbol.Create("KHC", SecurityType.Equity, Market.USA),
-        Symbol.Create("FDX", SecurityType.Equity, Market.USA),
+        Symbol.Create("SBSW", SecurityType.Equity, Market.USA),
+        Symbol.Create("GS", SecurityType.Equity, Market.USA),
+        Symbol.Create("HD", SecurityType.Equity, Market.USA),
+        Symbol.Create("AZN", SecurityType.Equity, Market.USA),
+        Symbol.Create("BABA", SecurityType.Equity, Market.USA),
+        Symbol.Create("BLK", SecurityType.Equity, Market.USA),
         Symbol.Create("KMB", SecurityType.Equity, Market.USA)]
         self.SetUniverseSelection( ManualUniverseSelectionModel(symbols) )
 
